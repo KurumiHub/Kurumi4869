a88af20361a2ef02e0a81f82c42ff3bbc2328934
KurumiHub <kurumiyosuki@gmail.com>
2024-06-24 22:03:18 +0700

    Update LoadG.lua

--- LoadG.lua
+++ LoadG.lua
@@ -1,5 +1,5 @@
 local gamelist = {
-        [2961111240] = "https://raw.githubusercontent.com/KurumiHub/Kurumi4869/main/MG___.lua"
+        [2961111240] = "https://raw.githubusercontent.com/KurumiHub/Kurumi4869/main/MG____G.lua"
     }
     local checkgame = gamelist[game.gameId]
 
