2f869ec5ae31e694e678881bb69c2f183356d783
KurumiHub <kurumiyosuki@gmail.com>
2024-06-26 11:03:54 +0700

    Update LoadW.lua

--- LoadW.lua
+++ LoadW.lua
@@ -1,6 +1,6 @@
 local gamelist = {
         [2961111240] = "https://raw.githubusercontent.com/KurumiHub/Kurumi4869/main/MonsterGhoul/MG____W.lua",
-        [5421640482]= "https://raw.githubusercontent.com/KurumiHub/Kurumi4869/main/JujutsuLegacy/JujutsuLegacy_W.lua"
+        [5421640482] = "https://raw.githubusercontent.com/KurumiHub/Kurumi4869/main/JujutsuLegacy/JujutsuLegacy_W.lua"
     }
     local checkgame = gamelist[game.gameId]
 
