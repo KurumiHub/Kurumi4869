9ea880459b8881cf36eae7d86355d65112d4de5e
KurumiHub <kurumiyosuki@gmail.com>
2024-07-06 06:18:11 +0700

    Update LoadW.lua

--- LoadW.lua
+++ LoadW.lua
@@ -1,6 +1,7 @@
 local gamelist = {
         [2961111240] = "https://raw.githubusercontent.com/KurumiHub/Kurumi4869/main/MonsterGhoul/MG____W.lua",
-        [5421640482] = "https://raw.githubusercontent.com/KurumiHub/Kurumi4869/main/JujutsuLegacy/JJKLGX_WL.lua"
+        [5421640482] = "https://raw.githubusercontent.com/KurumiHub/Kurumi4869/main/JujutsuLegacy/JJKLGX_WL.lua",
+        [5879054260] = "https://raw.githubusercontent.com/KurumiHub/Kurumi4869/main/RockFruitSS2/Rock_W.lua"
     }
     local checkgame = gamelist[game.gameId]
 
